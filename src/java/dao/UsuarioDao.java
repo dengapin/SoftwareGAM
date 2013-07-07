71c5cad288ed183f452b0ac58e275819e27563fd
rpico <rpico2011@gmail.com>
2013-07-06 21:40:49 -0500

    Create UsuarioDao.java

---
+++ src/java/dao/UsuarioDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+import model.Usuario;
+
+/**
+ *
+ * @author User
+ */
+public interface UsuarioDao {
+    public Usuario findByUsuario(Usuario usuario);
+    public List<Usuario> findAll();
+    public boolean create(Usuario user);
+    public boolean update(Usuario user);
+    public boolean delete(Integer id);
+}
